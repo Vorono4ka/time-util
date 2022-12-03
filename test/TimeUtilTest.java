80c4ffea0be55eb8176d0bf432284b604dd54e65
Danila <crowo4ka@gmail.com>
2022-12-03 13:56:39 +0300

    test for custom TimeFormatter

--- test/TimeUtilTest.java
+++ test/TimeUtilTest.java
@@ -8,6 +8,13 @@ Test written with junit:5.8.1
 public class TimeUtilTest {
     @Test
     public void timeToString() {
+        TimeFormatter myOwnDateFormatter = new DateTimeFormatter() {
+            @Override
+            public boolean isZeroForbidden(int unitIndex) {
+                return unitIndex == 0 || unitIndex >= 4;
+            }
+        };
+
         assertEquals("1 hour 1 min 1 sec", TimeUtil.timeToString(3661));
         assertEquals("1 min 1 sec", TimeUtil.timeToString(61));
         assertEquals("1 sec", TimeUtil.timeToString(1));
@@ -21,5 +28,14 @@ public class TimeUtilTest {
         assertEquals("0ms", TimeUtil.timeToString(0, TimeUtil.getDateTimeFormatter()));
 
         assertEquals("1h:1s", TimeUtil.timeToString(3601, TimeUtil.getDateTimeFormatter()));
+
+        assertEquals("1h:1m:1s", TimeUtil.timeToString(3661, myOwnDateFormatter));
+        assertEquals("0h:1m:1s", TimeUtil.timeToString(61, myOwnDateFormatter));
+        assertEquals("0h:0m:1s", TimeUtil.timeToString(1, myOwnDateFormatter));
+        assertEquals("0h:0m:0s", TimeUtil.timeToString(0, myOwnDateFormatter));
+
+        assertEquals("1h:0m:1s", TimeUtil.timeToString(3601, myOwnDateFormatter));
+        assertEquals("1d:0h:0m:0s", TimeUtil.timeToString(86400, myOwnDateFormatter));
+        assertEquals("1d:1h:0m:1s", TimeUtil.timeToString(90001, myOwnDateFormatter));
     }
 }
