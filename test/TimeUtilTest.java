d967e41b3842dccfedb85541c9961d670f2432b3
Danila <crowo4ka@gmail.com>
2022-12-03 14:35:37 +0300

    TimeUtilTest improvement: added a test for the day, different formatters are divided into different tests
    Note! Due to the type of seconds argument (float), milliseconds with a large number of seconds are 0

--- test/TimeUtilTest.java
+++ test/TimeUtilTest.java
@@ -6,8 +6,44 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 Test written with junit:5.8.1
  */
 public class TimeUtilTest {
+    // Units as seconds
+    private static final int aDay = 86400;
+    private static final int anHour = 3600;
+    private static final int aMinute = 60;
+    private static final int aSecond = 1;
+    private static final float aMillisecond = 0.001f;
+
+    private static final float testValue1 = aDay + anHour + aMinute + aSecond + aMillisecond;
+    private static final int testValue2 = anHour + aSecond;
+
     @Test
-    public void timeToString() {
+    public void timeToStringDefaultFormatter() {
+        assertEquals("0 ms", TimeUtil.timeToString(0));
+        assertEquals("1 ms", TimeUtil.timeToString(aMillisecond));
+        assertEquals("1 sec", TimeUtil.timeToString(aSecond));
+        assertEquals("1 min", TimeUtil.timeToString(aMinute));
+        assertEquals("1 hour", TimeUtil.timeToString(anHour));
+        assertEquals("1 day", TimeUtil.timeToString(aDay));
+        assertEquals("1 day 1 hour 1 min 1 sec", TimeUtil.timeToString(testValue1));
+
+        assertEquals("1 hour 1 sec", TimeUtil.timeToString(testValue2));
+    }
+
+    @Test
+    public void timeToStringDateFormatter() {
+        assertEquals("0ms", TimeUtil.timeToString(0, TimeUtil.getDateTimeFormatter()));
+        assertEquals("1ms", TimeUtil.timeToString(aMillisecond, TimeUtil.getDateTimeFormatter()));
+        assertEquals("1s", TimeUtil.timeToString(aSecond, TimeUtil.getDateTimeFormatter()));
+        assertEquals("1m", TimeUtil.timeToString(aMinute, TimeUtil.getDateTimeFormatter()));
+        assertEquals("1h", TimeUtil.timeToString(anHour, TimeUtil.getDateTimeFormatter()));
+        assertEquals("1d", TimeUtil.timeToString(aDay, TimeUtil.getDateTimeFormatter()));
+        assertEquals("1d:1h:1m:1s", TimeUtil.timeToString(testValue1, TimeUtil.getDateTimeFormatter()));
+
+        assertEquals("1h:1s", TimeUtil.timeToString(testValue2, TimeUtil.getDateTimeFormatter()));
+    }
+
+    @Test
+    public void timeToStringOwnFormatter() {
         TimeFormatter myOwnDateFormatter = new DateTimeFormatter() {
             @Override
             public boolean isZeroForbidden(int unitIndex) {
@@ -15,27 +51,16 @@ public class TimeUtilTest {
             }
         };
 
-        assertEquals("1 hour 1 min 1 sec", TimeUtil.timeToString(3661));
-        assertEquals("1 min 1 sec", TimeUtil.timeToString(61));
-        assertEquals("1 sec", TimeUtil.timeToString(1));
-        assertEquals("0 ms", TimeUtil.timeToString(0));
-
-        assertEquals("1 hour 1 sec", TimeUtil.timeToString(3601));
-
-        assertEquals("1h:1m:1s", TimeUtil.timeToString(3661, TimeUtil.getDateTimeFormatter()));
-        assertEquals("1m:1s", TimeUtil.timeToString(61, TimeUtil.getDateTimeFormatter()));
-        assertEquals("1s", TimeUtil.timeToString(1, TimeUtil.getDateTimeFormatter()));
-        assertEquals("0ms", TimeUtil.timeToString(0, TimeUtil.getDateTimeFormatter()));
-
-        assertEquals("1h:1s", TimeUtil.timeToString(3601, TimeUtil.getDateTimeFormatter()));
-
-        assertEquals("1h:1m:1s", TimeUtil.timeToString(3661, myOwnDateFormatter));
-        assertEquals("0h:1m:1s", TimeUtil.timeToString(61, myOwnDateFormatter));
-        assertEquals("0h:0m:1s", TimeUtil.timeToString(1, myOwnDateFormatter));
         assertEquals("0h:0m:0s", TimeUtil.timeToString(0, myOwnDateFormatter));
+        assertEquals("0h:0m:0s:1ms", TimeUtil.timeToString(aMillisecond, myOwnDateFormatter));
+        assertEquals("0h:0m:1s", TimeUtil.timeToString(aSecond, myOwnDateFormatter));
+        assertEquals("0h:1m:0s", TimeUtil.timeToString(aMinute, myOwnDateFormatter));
+        assertEquals("1h:0m:0s", TimeUtil.timeToString(anHour, myOwnDateFormatter));
+        assertEquals("1d:0h:0m:0s", TimeUtil.timeToString(aDay, myOwnDateFormatter));
+        assertEquals("1d:1h:1m:1s", TimeUtil.timeToString(testValue1, myOwnDateFormatter));
 
-        assertEquals("1h:0m:1s", TimeUtil.timeToString(3601, myOwnDateFormatter));
-        assertEquals("1d:0h:0m:0s", TimeUtil.timeToString(86400, myOwnDateFormatter));
-        assertEquals("1d:1h:0m:1s", TimeUtil.timeToString(90001, myOwnDateFormatter));
+        assertEquals("1h:0m:1s", TimeUtil.timeToString(testValue2, myOwnDateFormatter));
+
+        assertEquals("1d:1h:0m:1s", TimeUtil.timeToString(aDay + anHour + aSecond, myOwnDateFormatter));
     }
 }
